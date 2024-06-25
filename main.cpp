de982a778a99ce570c81cec6ab908ec444d673a5
Aybss <69048593+Dedoyes@users.noreply.github.com>
2024-06-25 14:38:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-const int MAXN = 1e6 + 5;
+const int MAXN = 1e4 + 5;
 int n, arr[MAXN];
 #define LL long long
 #define mod %
 #include "turing.h"
 #include "recursive.h"
+#include "functionStack.h"
 
 using namespace std;
 
@@ -23,12 +24,42 @@ int main() {
     cin >> n;
     for (int i = 1; i <= n; i++) {
         cin >> arr[i];
-        turing.arr[i] = tuElement (1, arr[i], "");
+    }
+    sort (arr + 1, arr + n + 1);
+    for (int i = 1; i <= n; i++)
+        cout << arr[i] << endl;
+    for (int i = 1; i <= n; i++) {
+        turing.arr[i].type = 1;
+        turing.arr[i].num = arr[i];
+    }
+    turing.arr[n + 1] = tuElement (0, 0, "$");
+    turing.arr[n + 2] = tuElement (1, 1, "");
+    turing.arr[n + 3] = tuElement (1, n, "");
+    turing.arr[n + 4] = tuElement (0, 0, "$");
+    TuringMachine Origin = turing;
+    turing.cnt = n + 4;
+    turing.print ();
+    while (true) {
+        turing = Origin;
+        int findX;
+        cin >> findX;
+        if (findX == -1)
+            break;
+        turing.cnt = n + 5;
+        turing.arr[n + 5] = tuElement (1, findX, "");
+        turing.print ();
+        turing.binSearch (findX);
+        cout << "movTot = " << turing.moveTot << endl;
     }
     while (true) {
         int findX;
         cin >> findX;
-        sys.execute (findX);
+        if (findX == -1)
+            break;
+        int findPos = sys.execute (findX);
+        if (findPos == -1) {
+            cout << "Don't find " << findX << endl;
+        }
     }
     return 0;
 }
