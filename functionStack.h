f58654e6627b3394afa501a39d61415928e3ad6a
Aybss <69048593+Dedoyes@users.noreply.github.com>
2024-06-25 16:02:06 +0800

    Update functionStack.h

--- functionStack.h
+++ functionStack.h
@@ -1,16 +1,21 @@
 #ifndef TEAM_ALGORITHM_FUNCTIONSTACK_H
 #define TEAM_ALGORITHM_FUNCTIONSTACK_H
 
-struct Function {
-    std::string name;
+inline int getLen (int x) {
+    int tot = 0;
+    while (x) {
+        x /= 10;
+        tot++;
+    }
+    tot = std::max (tot, (int)1);
+    return tot;
+}
 
-};
-
-struct funcStack {
+struct funcElement {
     std::string name;
     std::vector <int> vec;
-    funcStack () {}
-    funcStack (std::string _name, std::vector <int> _vec) {
+    funcElement () {}
+    funcElement (std::string _name, std::vector <int> _vec) {
         name = _name;
         for (auto x : _vec)
             vec.push_back (x);
@@ -28,9 +33,101 @@ struct funcStack {
     inline int memSize () {
         return sizeof (int) * vec.size ();
     }
-    inline int len () {
-
+    inline int width () {
+        int ret = 0;
+        ret += name.size ();
+        ret += 2;
+        for (auto x : vec) {
+            ret += getLen (x);
+            ret += 2;
+        }
+        return ret;
     }
 };
+
+struct funcStack {
+    std::vector <funcElement> vec;
+    funcStack () {}
+    funcStack (std::vector <funcElement> _vec) {
+        for (auto x : _vec)
+            this->vec.push_back (x);
+    }
+    inline void print () {
+        int maxLen = 0;
+        for (auto x : vec) {
+            maxLen = std::max (maxLen, x.width ());
+        }
+        std::cout << "| ";
+        for (int i = 1; i <= maxLen; i++)
+            std::cout << " ";
+        std::cout << " |" << std::endl;
+        for (auto it = vec.rbegin(); it != vec.rend (); it++) {
+            auto x = *it;
+            std::cout << "|";
+            for (int i = 1; i <= maxLen + 2; i++)
+                std::cout << "_";
+            std::cout << "|" << std::endl;
+            std::cout << "|";
+            int wide = x.width ();
+            int spaceLen = (maxLen + 2 - wide) / 2;
+            for (int i = 1; i <= spaceLen; i++) {
+                std::cout << " ";
+            }
+            x.print ();
+            for (int i = 1; i <= maxLen + 2 - spaceLen - wide; i++) {
+                std::cout << " ";
+            }
+            std::cout << "|" << std::endl;
+        }
+        for (int i = 1; i <= maxLen + 4; i++) {
+            std::cout << "-";
+        }
+        std::cout << std::endl;
+    }
+    inline bool empty () {
+        return this->vec.empty ();
+    }
+    funcElement top () {
+        return vec[vec.size () - 1];
+    }
+    void push (funcElement x) {
+        std::cout << "push" << std::endl;
+        vec.push_back (x);
+        this->print ();
+    }
+    void pop () {
+        std::cout << "pop" << std::endl;
+        vec.pop_back ();
+        this->print ();
+    }
+    int executeBin (int L, int R, int x) {
+        std::vector <int> tempVec;
+        tempVec.push_back (L);
+        tempVec.push_back (R);
+        tempVec.push_back (x);
+        funcElement fE ((std::string)"binSearch", tempVec);
+        this->push (fE);
+        int ret = -1;
+        while (!this->empty ()) {
+            auto nowd = this->top ();
+            this->pop ();
+            if (nowd.vec[0] > nowd.vec[1])
+                return -1;
+            int mid = (nowd.vec[0] + nowd.vec[1]) >> 1;
+            if (arr[mid] == nowd.vec[2]) {
+                return mid;
+            } else if (arr[mid] < x) {
+                auto newd = nowd;
+                newd.vec[0] = mid + 1;
+                this->push (newd);
+            } else {
+                auto newd = nowd;
+                newd.vec[1] = mid - 1;
+                this->push (newd);
+            }
+        }
+        return -1;
+    }
+} FuncStack;
 
 #endif //TEAM_ALGORITHM_FUNCTIONSTACK_H
